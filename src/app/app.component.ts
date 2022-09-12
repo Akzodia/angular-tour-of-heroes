e6bdbc79451b85b75c21a5bc0b158ae8be694151
Akzodiac <mehmetakz42@gmail.com>
2022-09-12 08:48:28 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'Tour of heroes';
+
+
+  myOberservable = new Observable((observer) => {
+    console.log("Observable starts")
+    setTimeout(() =>{observer.next("1")},1000);
+    setTimeout(() =>{observer.next("2")},2000);
+    setTimeout(() =>{observer.next("3")},3000);
+    setTimeout(() =>{observer.next("4")},4000);
+    setTimeout(() =>{observer.next("5")},5000);
+  });
+
+  ngOnInit(){
+    this.myOberservable.subscribe((val)=> {
+      console.log(val);
+    });
+  }
+}
